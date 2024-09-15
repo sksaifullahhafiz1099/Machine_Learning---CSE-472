f715725190834dbbdd173f777ea7569b691c26d1
Sk . Saifullah Hafiz <sksaifullahhafiz@gmail.com>
2024-09-15 22:37:35 +0600

    initial commit

---
+++ Offline-2/Data_Preprocessing.py
@@ -0,0 +1,107 @@
+# importing pandas csv library:
+import pandas as pd
+
+def data_import(file_name):
+    # stroing csv in variable:
+    dataframe = pd.read_csv(file_name)
+
+    # number of duplicate values in the dataset
+    dataframe[dataframe.duplicated(keep=False)]
+
+    #return dataframe
+    return dataframe
+
+def data_cleaning(dataframe):
+    # dropping row if target is null
+    dataframe.dropna(subset=['Attrition'], inplace=True)
+
+    # replacing any null number values with columnwise mean:
+    dataframe.fillna(dataframe.select_dtypes(include=['number']).mean(), inplace=True)
+
+    # replacing any null object values with columnwise most frequent:
+    for column in dataframe.select_dtypes(include=['object']).columns:
+        most_frequent = dataframe[column].mode()[0]
+        dataframe[column].fillna(most_frequent, inplace=True)
+
+    # keeping one copy of row if duplicate row found
+    dataframe.drop_duplicates(inplace=True)
+    
+    #return dataframe
+    return dataframe
+
+def input_output_feature_creation(dataframe):
+    # splitting data between Features and Labels
+    Features = dataframe.drop('Attrition', axis=1)
+    Labels = dataframe['Attrition']
+
+    #return
+    return Features,Labels
+
+def numeric_conversion(Features):
+    #using label encoding for some binary value columns
+    from sklearn.preprocessing import LabelEncoder
+
+    # Initialize LabelEncoder
+    label_encoder = LabelEncoder()
+
+    # Iterate over the object columns and apply label encoding to columns with two unique values
+    for col in Features.select_dtypes(include=['object']).columns:
+        if Features[col].nunique() == 2:  # Check if the column has only two unique values
+            Features[col] = label_encoder.fit_transform(Features[col])
+
+    # List of columns to convert to categorical
+    categorical_columns = Features.select_dtypes(include=['object']).columns.tolist()
+
+    # Convert non numeric columns to categorical type
+    for col in categorical_columns:
+        Features[col] = Features[col].astype('category')
+
+    # One hot encoding
+    Features = pd.get_dummies(Features)
+
+    # Convert only boolean columns back to 0 and 1
+    boolean_columns = Features.select_dtypes(include=['bool']).columns
+
+    # Convert the selected boolean columns to integers (0 and 1)
+    Features[boolean_columns] = Features[boolean_columns].astype(int)
+
+    #return
+    return Features
+
+def scaling(Features, scaling_type):
+    # Identify binary columns (columns with only two unique values)
+    binary_columns = [col for col in Features.columns if len(Features[col].unique()) == 2]
+    
+    # Separate binary and non-binary columns
+    non_binary_columns = Features.columns.difference(binary_columns)
+    
+    if scaling_type == "minmax":
+        from sklearn.preprocessing import MinMaxScaler
+        scaler = MinMaxScaler()
+    elif scaling_type == "standard":
+        from sklearn.preprocessing import StandardScaler
+        scaler = StandardScaler()
+
+    # Apply scaling only to non-binary columns
+    Features_scaled = Features.copy()
+    Features_scaled[non_binary_columns] = scaler.fit_transform(Features[non_binary_columns])
+    
+    return Features_scaled
+
+def correlation_analysis(features_normalized,Features,Labels):
+    # features and labels are put into dataframe
+    features_df = pd.DataFrame(features_normalized, columns=Features.columns) # scaled feature dataframe
+    labels_df = pd.DataFrame(Labels, columns=['Attrition'])
+
+    # correlation analysis of features with target
+    # Convert 'Yes' to 1 and 'No' to 0 in the target column
+    labels_df['Attrition'] = labels_df['Attrition'].map({'Yes': 1, 'No': 0})
+    target_series = labels_df['Attrition']
+    correlations = features_df.corrwith(target_series) #contribution of each column
+
+    #selecting top 20 correlations
+    top_20_correlations = correlations.abs().sort_values(ascending=False).head(20)
+
+    return top_20_correlations
+
+
