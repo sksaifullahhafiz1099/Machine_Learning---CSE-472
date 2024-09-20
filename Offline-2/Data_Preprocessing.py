132f48f1416d65479cd598d09bbb41b6760c5567
Sk . Saifullah Hafiz <sksaifullahhafiz@gmail.com>
2024-09-20 19:20:19 +0600

    final

--- Offline-2/Data_Preprocessing.py
+++ Offline-2/Data_Preprocessing.py
@@ -3,128 +3,130 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def data_import(file_name):
-    # stroing csv in variable:
-    dataframe = pd.read_csv(file_name)
+class Data_Prep:
+    def __init__(self,Target_column):
+        self.Target_column = Target_column
 
-    # number of duplicate values in the dataset
-    dataframe[dataframe.duplicated(keep=False)]
+    def data_import(self,file_name):
+        # stroing csv in variable:
+        dataframe = pd.read_csv(file_name)
 
-    #return dataframe
-    return dataframe
+        # number of duplicate values in the dataset
+        dataframe[dataframe.duplicated(keep=False)]
 
-def data_cleaning(dataframe):
-    # dropping row if target is null
-    dataframe.dropna(subset=['Attrition'], inplace=True)
+        #return dataframe
+        return dataframe
 
-    # replacing any null number values with columnwise mean:
-    dataframe.fillna(dataframe.select_dtypes(include=['number']).mean(), inplace=True)
+    def data_cleaning(self,dataframe):
+        # dropping row if target is null
+        dataframe.dropna(subset=[self.Target_column], inplace=True)
 
-    # replacing any null object values with columnwise most frequent:
-    for column in dataframe.select_dtypes(include=['object']).columns:
-        most_frequent = dataframe[column].mode()[0]
-        dataframe[column].fillna(most_frequent, inplace=True)
+        # replacing any null number values with columnwise mean:
+        dataframe.fillna(dataframe.select_dtypes(include=['number']).mean(), inplace=True)
 
-    # keeping one copy of row if duplicate row found
-    dataframe.drop_duplicates(inplace=True)
+        # replacing any null object values with columnwise most frequent:
+        for column in dataframe.select_dtypes(include=['object']).columns:
+            most_frequent = dataframe[column].mode()[0]
+            dataframe[column].fillna(most_frequent, inplace=True)
+
+        # keeping one copy of row if duplicate row found
+        dataframe.drop_duplicates(inplace=True)
+        
+        #return dataframe
+        return dataframe
+
+    def input_output_feature_creation(self,dataframe):
+        # splitting data between Features and Labels
+        Features = dataframe.drop(self.Target_column, axis=1)
+        Labels = dataframe[self.Target_column]
     
-    #return dataframe
-    return dataframe
+        #return
+        return Features,Labels
 
-def input_output_feature_creation(dataframe):
-    # splitting data between Features and Labels
-    Features = dataframe.drop('Attrition', axis=1)
-    Labels = dataframe['Attrition']
+    def numeric_conversion(self,Features):
+        #using label encoding for some binary value columns
+        from sklearn.preprocessing import LabelEncoder
 
-    #return
-    return Features,Labels
+        # Initialize LabelEncoder
+        label_encoder = LabelEncoder()
 
-def numeric_conversion(Features):
-    #using label encoding for some binary value columns
-    from sklearn.preprocessing import LabelEncoder
+        # Iterate over the object columns and apply label encoding to columns with two unique values
+        for col in Features.select_dtypes(include=['object']).columns:
+            if Features[col].nunique() == 2:  # Check if the column has only two unique values
+                Features[col] = label_encoder.fit_transform(Features[col])
 
-    # Initialize LabelEncoder
-    label_encoder = LabelEncoder()
+        # List of columns to convert to categorical
+        categorical_columns = Features.select_dtypes(include=['object']).columns.tolist()
 
-    # Iterate over the object columns and apply label encoding to columns with two unique values
-    for col in Features.select_dtypes(include=['object']).columns:
-        if Features[col].nunique() == 2:  # Check if the column has only two unique values
-            Features[col] = label_encoder.fit_transform(Features[col])
+        # Convert non numeric columns to categorical type
+        for col in categorical_columns:
+            Features[col] = Features[col].astype('category')
 
-    # List of columns to convert to categorical
-    categorical_columns = Features.select_dtypes(include=['object']).columns.tolist()
+        # One hot encoding
+        Features = pd.get_dummies(Features)
 
-    # Convert non numeric columns to categorical type
-    for col in categorical_columns:
-        Features[col] = Features[col].astype('category')
+        # Convert only boolean columns back to 0 and 1
+        boolean_columns = Features.select_dtypes(include=['bool']).columns
 
-    # One hot encoding
-    Features = pd.get_dummies(Features)
+        # Convert the selected boolean columns to integers (0 and 1)
+        Features[boolean_columns] = Features[boolean_columns].astype(int)
 
-    # Convert only boolean columns back to 0 and 1
-    boolean_columns = Features.select_dtypes(include=['bool']).columns
+        #return
+        return Features
 
-    # Convert the selected boolean columns to integers (0 and 1)
-    Features[boolean_columns] = Features[boolean_columns].astype(int)
+    def scaling(self,Features, scaling_type):
+        # Identify binary columns (columns with only two unique values)
+        binary_columns = [col for col in Features.columns if len(Features[col].unique()) == 2]
+        
+        # Separate binary and non-binary columns
+        non_binary_columns = Features.columns.difference(binary_columns)
+        
+        if scaling_type == "minmax":
+            from sklearn.preprocessing import MinMaxScaler
+            scaler = MinMaxScaler()
+        elif scaling_type == "standard":
+            from sklearn.preprocessing import StandardScaler
+            scaler = StandardScaler()
 
-    #return
-    return Features
+        # Apply scaling only to non-binary columns
+        Features_scaled = Features.copy()
+        Features_scaled[non_binary_columns] = scaler.fit_transform(Features[non_binary_columns])
+        
+        return Features_scaled
 
-def scaling(Features, scaling_type):
-    # Identify binary columns (columns with only two unique values)
-    binary_columns = [col for col in Features.columns if len(Features[col].unique()) == 2]
-    
-    # Separate binary and non-binary columns
-    non_binary_columns = Features.columns.difference(binary_columns)
-    
-    if scaling_type == "minmax":
-        from sklearn.preprocessing import MinMaxScaler
-        scaler = MinMaxScaler()
-    elif scaling_type == "standard":
-        from sklearn.preprocessing import StandardScaler
-        scaler = StandardScaler()
+    def correlation_analysis(self,features_normalized,Features,Labels):
+        # features and labels are put into dataframe
+        features_df = pd.DataFrame(features_normalized, columns=Features.columns) # scaled feature dataframe
+        labels_df = pd.DataFrame(Labels, columns=[self.Target_column])
 
-    # Apply scaling only to non-binary columns
-    Features_scaled = Features.copy()
-    Features_scaled[non_binary_columns] = scaler.fit_transform(Features[non_binary_columns])
-    
-    return Features_scaled
+        # correlation analysis of features with target
+        # Convert 'Yes' to 1 and 'No' to 0 in the target column
+        labels_df[self.Target_column] = labels_df[self.Target_column].map({'Yes': 1, 'No': 0})
+        target_series = labels_df[self.Target_column]
+        correlations = features_df.corrwith(target_series) #contribution of each column
 
-def correlation_analysis(features_normalized,Features,Labels):
-    # features and labels are put into dataframe
-    features_df = pd.DataFrame(features_normalized, columns=Features.columns) # scaled feature dataframe
-    labels_df = pd.DataFrame(Labels, columns=['Attrition'])
+        #selecting top 20 correlations
+        top_20_correlations = correlations.abs().sort_values(ascending=False).head(20)
 
-    # correlation analysis of features with target
-    # Convert 'Yes' to 1 and 'No' to 0 in the target column
-    labels_df['Attrition'] = labels_df['Attrition'].map({'Yes': 1, 'No': 0})
-    target_series = labels_df['Attrition']
-    correlations = features_df.corrwith(target_series) #contribution of each column
+        return top_20_correlations,features_df,labels_df
 
-    #selecting top 20 correlations
-    top_20_correlations = correlations.abs().sort_values(ascending=False).head(20)
+    def show_plot(self,top_20_correlations,features_df,labels_df):
+        # Assuming 'correlations' is a Pandas Series with feature correlation values
+        # Sort correlations and get the top 20
+        top_20_features = top_20_correlations.index
 
-    return top_20_correlations,features_df,labels_df
+        # Loop through each of the top 20 features
+        for feature in top_20_features:
+            # Separate the data based on the Attrition class
+            class_0 = features_df[labels_df[self.Target_column] == 0]
+            class_1 = features_df[labels_df[self.Target_column] == 1]
 
-def show_plot(top_20_correlations,features_df,labels_df):
-    # Assuming 'correlations' is a Pandas Series with feature correlation values
-    # Sort correlations and get the top 20
-    top_20_features = top_20_correlations.index
+            # Plot 1D scatter plot
+            plt.figure(figsize=(8, 4))
+            plt.plot(class_0[feature], np.zeros_like(class_0[feature]), 'o', label=self.Target_column+' 0')
+            plt.plot(class_1[feature], np.zeros_like(class_1[feature]), 'o', label=self.Target_column+' 1')
 
-    # Loop through each of the top 20 features
-    for feature in top_20_features:
-        # Separate the data based on the Attrition class
-        class_0 = features_df[labels_df["Attrition"] == 0]
-        class_1 = features_df[labels_df["Attrition"] == 1]
-
-        # Plot 1D scatter plot
-        plt.figure(figsize=(8, 4))
-        plt.plot(class_0[feature], np.zeros_like(class_0[feature]), 'o', label='Attrition 0')
-        plt.plot(class_1[feature], np.zeros_like(class_1[feature]), 'o', label='Attrition 1')
-
-        plt.legend()
-        plt.xlabel(feature)
-        plt.title(f'1D Scatter Plot of {feature} by Attrition Classes')
-        plt.show()
-
-
+            plt.legend()
+            plt.xlabel(feature)
+            plt.title(f'1D Scatter Plot of {feature} by {self.Target_column} Classes')
+            plt.show()
